84fce6328b59823f942a024e94d80e46dbabb259
skandergod <thedanidacosta@gmail.com>
2025-01-03 23:49:10 -0400

    "First Push"

---
+++ Proyecto1AyP22024/src/main/java/com/mycompany/proyecto1ayp22024/Proyecto1AyP22024.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1ayp22024;
+
+/**
+ *
+ * @author theda
+ */
+public class Proyecto1AyP22024 {
+
+    public static void main(String[] args) {
+        System.out.println("First Commit!");
+        
+    }
+}
